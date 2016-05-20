0102e604eb20a9ff063b8052b1fc59029b74d9c9
Arthur Ferreira <arthur.ferreira@office.netbiis.com>
2016-05-20 14:53:06 -0300

    added FileUploadExample

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,3 +1,18 @@
+var uuid = require('node-uuid'),
+    multer = require('multer'),
+    fileName = '',
+    storage = multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, './media/users');
+        },
+        filename: function (req, file, callback) {
+            var ext = file.originalname.replace(/[\S\s]+?\.([\S]+)$/g, '$1');
+            fileName = `${uuid.v1()}.${ext}`;
+            callback(null, fileName);
+        }
+    }),
+    upload = multer({ storage: storage }).single('profileImage');;
+
 module.exports = function (app) {
     var User = app.models.user;
     return {
@@ -24,15 +39,19 @@ module.exports = function (app) {
             });
         },
         update: function (req, res) {
-            User.findByIdAndUpdate(req.params.userId,
-                { $set: req.body },
-                { new: true },
-                function (err, data) {
-                    if (err) throw err;
-                    if (!data) { res.sendStatus(404); return; }
-                    res.json(data);
+            upload(req, res, function (err) {
+                if (!err) req.body.profile_image = fileName;
+                fileName = "";
+                User.findByIdAndUpdate(req.params.userId,
+                    { $set: req.body },
+                    { new: true },
+                    function (err, data) {
+                        if (err) throw err;
+                        if (!data) { res.sendStatus(404); return; }
+                        res.json(data);
 
-                });
+                    });
+            });
         },
         destroy: function (req, res) {
             User.findByIdAndRemove(req.params.userId, function (err, data) {
